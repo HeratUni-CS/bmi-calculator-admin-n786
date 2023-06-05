e8a554b168fb48088bcc37b59968a25337b4e75a
admin-n786 <movahhedd786@gmail.com>
2023-06-05 13:13:30 -0700

    second commit

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const KLabelstyle = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w400,
+  color: Colors.blue,
+);
+
+
+
+const KResualtTextStyle=TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF24D876),
+);
+
+const KBMITextStyle= TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.bold,
+);
+const KBodyTextStyle=TextStyle(
+  fontSize: 22,
+);
+const KLargeButtonStyle = TextStyle(fontWeight: FontWeight.bold,fontSize: 20);
+const KStylenumber=TextStyle(fontSize: 50);
+const KBottomcontainercolor= Color(0xDFFFFFF);
+const KActivecolor= Color(0xBEC2C5D5);
+const KInactivecolor=Color(0x8FFFFFF);
